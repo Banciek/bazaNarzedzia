b4c2ddb59268b1440dae73032a1bcd6ad4855c21
Banciek <danqzz@gmail.com>
2016-03-11 18:36:23 +0100

    Added session handlig with remember users (storing session data in cookies)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,14 @@ class SessionsController < ApplicationController
   
 
   def new
-  	
+  	redirect_to current_user if logged_in? 
   end
 
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
   		log_in(user)
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
   		redirect_to user
   	else
   		flash.now[:danger] = 'Błędny email lub hasło'
@@ -17,7 +18,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-  	log_out
+  	log_out if logged_in?
   	redirect_to root_path
   	flash[:info] = 'Zostałeś poprawnie wylogowany'
   end
